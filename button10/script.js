7521d1e8999361edfdf62d0a58bfb6808bd34719
shaun045 <shaun1adam2@gmail.com>
2026-04-21 15:44:38 +0800

    Added script statement for lightmode-darkmode

--- button10/script.js
+++ button10/script.js
@@ -13,4 +13,10 @@ toggle.addEventListener("click", () => {
 
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light Mode"
+  } else {
+    modeText.innerText = "Dark Mode"
+  }
 });
