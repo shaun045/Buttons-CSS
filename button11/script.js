de461a0cf4fe478dad89c15ff6d1bdd9eb41d957
shaun045 <shaun1adam2@gmail.com>
2026-05-01 15:51:01 +0800

    added JS script for sidebar toggle button rotate

--- button11/script.js
+++ button11/script.js
@@ -1,3 +1,11 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+  sidebar.classList.toggle('close');
+  toggleButton.classList.toggle('rotate');
+}
+
 function toggleSubMenu(button) {
   button.nextElementSibling.classList.toggle('show');
   button.classList.toggle('rotate');
