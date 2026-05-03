aa81999f3ecee7a323da72c67a52a39351c643b0
shaun045 <shaun1adam2@gmail.com>
2026-05-03 12:38:13 +0800

    added sidebar menu script

--- button11/script.js
+++ button11/script.js
@@ -9,4 +9,9 @@ function toggleSidebar() {
 function toggleSubMenu(button) {
   button.nextElementSibling.classList.toggle('show');
   button.classList.toggle('rotate');
+
+  if (sidebar.classList.contains('close')) {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+  }
 }
