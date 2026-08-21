c33f46fdb48c30014bfe10decfb8ad76665559d5
shaun045 <shaun1adam2@gmail.com>
2026-08-22 06:10:48 +0800

    Saving toggle themes to localstorage

--- button13/main.js
+++ button13/main.js
@@ -5,7 +5,9 @@ const themeBtn = document.querySelector(".theme__btn");
 themeBtn.addEventListener("click", () => {
   if (document.body.className == '') {
     document.body.className = 'dark'
+    localStorage.setItem('portfolio__theme', 'dark');
   } else {
     document.body.className = ''
+    localStorage.setItem('portfolio__theme', '');
   }
 })
