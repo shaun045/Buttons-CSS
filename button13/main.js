48e65971bfb1bf16b1be9b29a33bec51467b6762
shaun045 <shaun1adam2@gmail.com>
2026-08-22 06:08:01 +0800

    darkmode application, toggle

--- button13/main.js
+++ button13/main.js
@@ -3,5 +3,9 @@
 // CHANGE AND SAVE THEME TO LOCAL STORAGE
 const themeBtn = document.querySelector(".theme__btn");
 themeBtn.addEventListener("click", () => {
-  
+  if (document.body.className == '') {
+    document.body.className = 'dark'
+  } else {
+    document.body.className = ''
+  }
 })
